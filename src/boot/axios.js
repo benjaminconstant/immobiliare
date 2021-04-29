aa5dff5d371644a556c360fd6fdea0ffdc2ef036
benjaminconstant <gianluca.const@gmail.com>
2021-04-29 18:21:47 +0200

    push

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.DEV ? 'http://localhost:8000/api/' : 'https://invernizzibackend.pythonanywhere.com/api/'
+  baseURL: process.env.DEV ? 'http://localhost:8000/api/' : 'https://immobiliare.pythonanywhere.com/api/'
   // headers: {'X-Custom-Header': 'foobar'}
 })
 Vue.prototype.$axios = api
