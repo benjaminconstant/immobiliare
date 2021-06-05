ac7178130120afa5080bd4648e0720f8cc9050cf
benjaminconstant <gianluca.const@gmail.com>
2021-06-05 12:13:16 +0200

    push

--- django/immobiliare/settings.py
+++ django/immobiliare/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
