11a73fd62aef3cbfc95c7744c9537f79b2f00fe3
benjaminconstant <gianluca.const@gmail.com>
2022-02-10 14:51:37 +0100

    push

--- django/immobiliare/settings.py
+++ django/immobiliare/settings.py
@@ -11,7 +11,7 @@ DEBUG = not PRODUCTION
 
 if PRODUCTION:
     print('Running in PRODUCTION mode')
-    ALLOWED_HOSTS = ['immobiliare.pythonanywhere.com']
+    ALLOWED_HOSTS = ['eu.immobiliare.pythonanywhere.com']
 else:
     print('Running in DEVELOPMENT mode')
     ALLOWED_HOSTS = ['*']
