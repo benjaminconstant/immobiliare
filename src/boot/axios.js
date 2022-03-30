e7b6a2ba7976bcd614ac88b89800c3a1fe31575c
benjaminconstant <gianluca.const@gmail.com>
2022-03-30 13:58:22 +0200

    push

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -5,7 +5,7 @@ import { Cookies } from 'quasar'
 const token = Cookies.get('csrftoken')
 
 export const api = axios.create({
-  baseURL: process.env.DEV ? 'http://localhost:8000/api/' : 'https://immobiliare.eu.pythonanywhere.com/api/',
+  baseURL: process.env.DEV ? 'http://localhost:8000/api/' : 'https://www.ghiga.top/api/',
   headers: { 'X-CSRFToken': token }
 })
 Vue.prototype.$axios = api
